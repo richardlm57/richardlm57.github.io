6b1dcb4dbbf61af338507b016d94bfdd840fedc0
richardlm57 <richardlm57@gmail.com>
2017-06-19 18:31:36 -0600

    Re-push final version

--- source.js
+++ source.js
@@ -4,14 +4,10 @@ function calculate(){
 	var endDate = new Date();
 	endDate.setTime(startDate.getTime() + days * 86400000 );
 	console.log(startDate)
-	console.log(endDate)
-	/*
+	console.log(endDate);
 	$("#output").datepicker({
 	    minDate: startDate,
 	    maxDate: endDate
 	});
 	$.datepicker.setDefaults( $.datepicker.regional[ $("#countryCode").val() ] );
-	*/
-	$("#calendar").clndr();
-
 }
